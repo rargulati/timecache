5f3fa0cedcea92e1272ccd0bebc382127fa6cd2c
Raghav Gulati <rargulati@users.noreply.github.com>
2018-09-11 17:23:03 -0700

    Add concurrency tests
    Tease out races in both the map and list by adding to the map (and list)
    concurrently.

--- timecache_test.go
+++ timecache_test.go
@@ -1,7 +1,9 @@
 package timecache
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -25,5 +27,52 @@ func TestEntriesExpire(t *testing.T) {
 
 	if tc.Has(fmt.Sprint(0)) {
 		t.Fatal("should have dropped this from the cache already")
+	}
+}
+
+func TestMapEntryStateRace(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	tc := NewTimeCache(time.Second)
+
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 10; i++ {
+			tc.Add(fmt.Sprint(i))
+			time.Sleep(time.Millisecond * 10)
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for i := 10; i < 20; i++ {
+			tc.Add(fmt.Sprint(i))
+			time.Sleep(time.Millisecond * 10)
+
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	select {
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	default:
+	}
+
+	for i := 0; i < 20; i++ {
+		if !(tc.Has(fmt.Sprint(i))) {
+			t.Fatalf("time cache missing expected element %s", fmt.Sprint(i))
+		}
+	}
+
+	select {
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	default:
 	}
 }
