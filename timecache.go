77bec64cfd160acfc1df894c2e46a8fd0fa2de52
Raghav Gulati <rargulati@users.noreply.github.com>
2018-09-11 17:24:14 -0700

    Fix race in map and list
    Guard both with their own lock. Though verbose, we ensure all paths are
    covered.

--- timecache.go
+++ timecache.go
@@ -2,12 +2,16 @@ package timecache
 
 import (
 	"container/list"
+	"sync"
 	"time"
 )
 
 type TimeCache struct {
-	Q *list.List
-	M map[string]time.Time
+	Q     *list.List
+	qLock sync.Mutex
+
+	M     map[string]time.Time
+	mLock sync.Mutex
 
 	span time.Duration
 }
@@ -21,40 +25,61 @@ func NewTimeCache(span time.Duration) *TimeCache {
 }
 
 func (tc *TimeCache) Add(s string) {
+	tc.mLock.Lock()
 	_, ok := tc.M[s]
 	if ok {
 		panic("putting the same entry twice not supported")
 	}
+	tc.mLock.Unlock()
 
 	tc.sweep()
 
+	tc.mLock.Lock()
 	tc.M[s] = time.Now()
+	tc.mLock.Unlock()
+
+	tc.qLock.Lock()
 	tc.Q.PushFront(s)
+	tc.qLock.Unlock()
 }
 
 func (tc *TimeCache) sweep() {
 	for {
+		tc.qLock.Lock()
 		back := tc.Q.Back()
 		if back == nil {
+			tc.qLock.Unlock()
 			return
 		}
+		tc.qLock.Unlock()
 
 		v := back.Value.(string)
+
+		tc.mLock.Lock()
 		t, ok := tc.M[v]
 		if !ok {
 			panic("inconsistent cache state")
 		}
+		tc.mLock.Unlock()
 
 		if time.Since(t) > tc.span {
+			tc.qLock.Lock()
 			tc.Q.Remove(back)
+			tc.qLock.Unlock()
+
+			tc.mLock.Lock()
 			delete(tc.M, v)
-		} else {
-			return
+			tc.mLock.Unlock()
 		}
+
+		return
 	}
 }
 
 func (tc *TimeCache) Has(s string) bool {
+	tc.mLock.Lock()
+	defer tc.mLock.Unlock()
+
 	_, ok := tc.M[s]
 	return ok
 }
